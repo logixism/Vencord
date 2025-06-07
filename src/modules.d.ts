b5afc4243133474727c13665172e1c0003dce453
logixism <logixism@pm.me>
2025-06-07 15:29:07 +0300

    exclude dev everywhere

--- src/modules.d.ts
+++ src/modules.d.ts
@@ -25,7 +25,7 @@ declare module "~plugins" {
         folderName: string;
         userPlugin: boolean;
     }>;
-    export const ExcludedPlugins: Record<string, "web" | "discordDesktop" | "vesktop" | "desktop" | "dev">;
+    export const ExcludedPlugins: Record<string, "web" | "discordDesktop" | "vesktop" | "desktop">;
 }
 
 declare module "~pluginNatives" {
